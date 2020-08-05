ab246b29c83791c94531f9db18c3dbe5cd7d5742
shenal pelpola <shenalpelpola12@gmail.com>
2020-08-05 19:01:14 +0530

    Updates

---
+++ precache-manifest.62463e7e977eacbe9797972b33e8021f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d663bb9022e7d04cbc83ba4ab7e874bd",
+    "url": "/react-scoreboard/index.html"
+  },
+  {
+    "revision": "eb13d1a4e8fc45665c5c",
+    "url": "/react-scoreboard/static/css/main.184198b1.chunk.css"
+  },
+  {
+    "revision": "85bd7b72acf97c20fa09",
+    "url": "/react-scoreboard/static/js/2.0be07452.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-scoreboard/static/js/2.0be07452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb13d1a4e8fc45665c5c",
+    "url": "/react-scoreboard/static/js/main.7099a02e.chunk.js"
+  },
+  {
+    "revision": "eda609f1a93fac9d53a0",
+    "url": "/react-scoreboard/static/js/runtime-main.3a7f9486.js"
+  }
+]);
